885930e4950684bbfc8cd996ca0d9225a7e31307
María <mariacorredoiramartinez@gmail.com>
2025-12-22 12:42:24 +0100

    "Initial project setup: Spring Boot project structure, dependencies, and configuration"

---
+++ src/test/java/com/mariacorredoira/gestorreservasback/GestorReservasBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariacorredoira.gestorreservasback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorReservasBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
